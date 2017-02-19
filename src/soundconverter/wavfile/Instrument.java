c0cfa0ef7c7a05558f5f727f571f014c160108b5
TxmszLou <tmzl.sx@gmail.com>
2017-02-18 23:53:24 -0800

    add piano

---
+++ src/soundconverter/wavfile/Instrument.java
@@ -0,0 +1,9 @@
+package soundconverter.wavfile;
+
+/**
+ * Created by txmszlou on 2/18/17.
+ */
+public abstract class Instrument {
+    public abstract double freq(int n);
+    public abstract String toString();
+}
